51cea2a53ae4ee0865ef8f1fc49f5c06ecfb2ff2
Michał Zagól <mysza1179@gmail.com>
2015-06-01 20:46:27 +0000

    changed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
 
   resources :projects
 
-  devise_for :users
+  devise_for :users, controllers: {registrations: "registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
